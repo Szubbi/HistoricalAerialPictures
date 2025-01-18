5dc056e03412ae7b515d3e753419e0590e24b039
Piotr Szubert <piotr.szubert@doctoral.uj.edu.pl>
2025-01-18 13:56:19 +0100

    Image converted rewitten to the class

--- GenerateDummyTrainingSample.py
+++ GenerateDummyTrainingSample.py
@@ -15,37 +15,54 @@ import numpy as np
 import skimage as ski
 
 
-orto_dir = '/media/pszubert/DANE/07_OneDriveBackup/05_PrzetwarzanieDawnychZdjec/01_InData/06_Orto'
-dst_dir = '/media/pszubert/DANE/07_OneDriveBackup/05_PrzetwarzanieDawnychZdjec/03_DataProcessing'
-img_dir = os.path.join(orto_dir, '79045_1296512_M-34-74-A-b-3-4.tif')
 
-with rio.open(img_dir) as src:
-    transform = src.transform
-    crs = src.crs
+class ImageConverter:
+    
+    def __init__(self, img_dir:str):
+        self.img_dir = img_dir
+        
+        
+    def convert_image(self, kernel_size:int):
+        self.img = cv2.imread(self.img_dir)
+        self.bw_img = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+        
+        self.kernel = np.ones((kernel_size, kernel_size),np.float32)/25
+        self.avg_img = cv2.filter2D(self.bw_img, -1, self.kernel)
+        self.blured_img = cv2.GaussianBlur(self.avg_img, (5,5), 0)
+        self.noise_img = ski.util.random_noise(self.blured_img, mode='gaussian')
+        
+        return self.noise_img
+        
+    
+    def save(self, dst_dir:str):
 
-with rio.open(os.path.join(orto_dir, 'bw_test.tif')) as src:
-    profile = src.profile
+        with rio.open(self.img_dir) as src:
+            transform = src.transform
+            crs = src.crs
+    
+        raster_meta = {'driver': 'GTiff',
+                       'dtype': self.noise_img.dtype, 
+                       'nodata': None,
+                       'height': self.noise_img.shape[0], 
+                       'width': self.noise_img.shape[1], 
+                       'count': 1, 
+                       'crs': crs, 
+                       'transform': transform}
+        
+        with rio.open(dst_img, 'w', **raster_meta) as dst:
+            dst.write(self.noise_img, 1)
+            
+            
+if __name__ == "__main__":
+    orto_dir = '/media/pszubert/DANE/07_OneDriveBackup/05_PrzetwarzanieDawnychZdjec/01_InData/06_Orto'
+    dst_dir = '/media/pszubert/DANE/07_OneDriveBackup/05_PrzetwarzanieDawnychZdjec/03_DataProcessing'
+    img_dir = os.path.join(orto_dir, '79045_1296512_M-34-74-A-b-3-4.tif')
+    dst_img = os.path.join(dst_dir, 'test_03.tif')
+    
+    ImageConv = ImageConverter(img_dir)
+    ImageConv.convert_image(5)
+    ImageConv.save(dst_img)
 
-img = cv2.imread(img_dir)
-bw_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-kernel = np.ones((5,5),np.float32)/25
-avg_img = cv2.filter2D(bw_img, -1, kernel)
-blured_img = cv2.GaussianBlur(avg_img, (5,5), 0)
-noise_img = ski.util.random_noise(blured_img, mode='gaussian') 
 
-plt.figure(figsize=(22,22))
-plt.imshow(noise_img, cmap='gray')
-plt.show()
 
-profile.update({ 'count': 1, 
-                'dtype': noise_img.dtype, 
-                'height': noise_img.shape[0], 
-                'width': noise_img.shape[1],
-                'crs': crs,
-                'transform': transform})
-
-dst_img = os.path.join(dst_dir, 'test_02.tif')
-
-with rio.open(dst_img, 'w', **profile) as dst:
-    dst.write(noise_img, 1)
---
+++ VisualizeComparison.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 18 13:25:56 2025
+
+@author: piotr.szubert@doctoral.uj.edu.pl
+
+"""
+
+def visualize_comparison(converted_img, target_img):
+    pass
